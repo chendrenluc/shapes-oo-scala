9fd58af51382f2fdf205c10a1c09b2151ee767a9
chendren <connorhendren24@gmail.com>
2026-09-16 12:04:35 -0500

    Added missing cases to shapes.scala Implemented boundingBox, might be some errors though Fixed the error in boundingBox, the values of width and height don't exist in the professors "Shape" import, so I altered the "Group" case to use the previous implementation of Location and Rectangle from shapes. Removed the comment brackets from all the tests to see if the program works now. Tests all passed for boundingBox, so now I created the 3 scala objects for size, height, and scale and initialized size. Initialized height. Initialized scale. Should mention; at this point AI has been used to ensure proper initialization of these 3 objects, and will be used some more now to test these objects and check the code Created all test suites for the 3 new objects and initialized TestSize. Initialized TestHeight Initialized TestScale Fixed an error Microsoft Copilot caught in the written tests for height where the tests expected "3" and "6" but I had written "2" and "4". Added logging to boundingBox.scala Added logging to size.scala Added logging to height.scala Added logging to scale.scala

--- src/main/scala/scale.scala
+++ src/main/scala/scale.scala
@@ -1,16 +1,22 @@
 package edu.luc.cs.laufer.cs371.shapes
 
+import java.util.logging.Logger
 import Shape.*
 
 object scale {
+  private val log = Logger.getLogger("shapes.scale")
   def apply(s: Shape, factor: Int): Shape = s match {
     case Rectangle(w, h) =>
+      log.fine(s"scale(Rectangle($w, $h), $factor)")
       Rectangle(w * factor, h * factor)
     case Ellipse(rx, ry) =>
+      log.fine(s"scale(Ellipse($rx, $ry), $factor)")
       Ellipse(rx * factor, ry * factor)
     case Location(x, y, inner) =>
+      log.fine(s"scale(Location($x, $y, ...), $factor)")
       Location(x * factor, y * factor, apply(inner, factor))
     case Group(shapes*) =>
+      log.fine(s"scale(Group with ${shapes.size} shapes, $factor)")
       Group(shapes.map(apply(_, factor))*)
   }
 }
