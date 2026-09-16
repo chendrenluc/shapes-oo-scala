e77fe34d59c9a427632105411f1a8d83ba4603a8
chendren <connorhendren24@gmail.com>
2026-09-16 10:49:24 -0500

    Added missing cases to shapes.scala Implemented boundingBox, might be some errors though Fixed the error in boundingBox, the values of width and height don't exist in the professors "Shape" import, so I altered the "Group" case to use the previous implementation of Location and Rectangle from shapes. Removed the comment brackets from all the tests to see if the program works now. Tests all passed for boundingBox, so now I created the 3 scala objects for size, height, and scale and initialized size. Initialized height. Initialized scale. Should mention; at this point AI has been used to ensure proper initialization of these 3 objects, and will be used some more now to test these objects and check the code Created all test suites for the 3 new objects and initialized TestSize. Initialized TestHeight

--- src/test/scala/TestHeight.scala
+++ src/test/scala/TestHeight.scala
@@ -1,3 +1,27 @@
-class TestHeight {
+package edu.luc.cs.laufer.cs371.shapes
 
+import org.scalatest.funsuite.AnyFunSuite
+import TestFixtures.*
+import height.*
+
+class TestHeight extends AnyFunSuite{
+  test("Simple Rectangle") {
+    assert(apply(simpleRectangle) == 1)
+  }
+  test("Simple Ellipse") {
+    assert(apply(simpleEllipse) == 1)
+  }
+  test("Simple Location") {
+    assert(apply(simpleLocation) == 2)
+  }
+  test("Basic Group") {
+    assert(apply(basicGroup) == 2)
+  }
+  test("Simple Group") {
+    assert(apply(simpleGroup) == 2)
+  }
+  test("Complex Group") {
+    assert(apply(complexGroup) == 4)
+  }
 }
+end TestHeight
