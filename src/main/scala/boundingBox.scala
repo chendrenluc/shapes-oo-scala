5141320ef55f4b69389e74d18b7e26d80c56074c
chendren <connorhendren24@gmail.com>
2026-09-16 09:06:39 -0500

    Added missing cases to shapes.scala Implemented boundingBox, might be some errors though

--- src/main/scala/boundingBox.scala
+++ src/main/scala/boundingBox.scala
@@ -6,6 +6,25 @@ import Shape.*
 
 object boundingBox:
   def apply(s: Shape): Location = s match
-    case _ => Location(0, 0, Rectangle(0, 0)) // not yet implemented
+    //Rectangle
+    case Rectangle(w, h) => 
+      Location(0, 0, Rectangle(w, h))
+    //Ellipse
+    case Ellipse(rx, ry) => 
+      Location(-rx, -ry, Rectangle(2 * rx, 2 * ry))
+    //Location
+    case Location(x, y, inner) => 
+      val Location(ix, iy, Rectangle(w, h)) = apply(inner)
+      Location(ix + x, iy + y, Rectangle(w, h))
+    //Group
+    case Group(shapes*) =>
+      val boxes = shapes.map(apply)
+      val minX = boxes.map(_.x).min
+      val minY = boxes.map(_.y).min
+      val maxX = boxes.map(b => b.x + b.shape.width).max
+      val maxY = boxes.map(b => b.y + b.shape.height).max
+      val width = maxX - minX
+      val height = maxY - minY
+      Location(minX, minY, Rectangle(width, height))
 
 end boundingBox
