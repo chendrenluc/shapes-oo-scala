fcf4189b3cff12d354dfa84618e66738f0a06c99
chendren <connorhendren24@gmail.com>
2026-09-16 11:54:15 -0500

    Added missing cases to shapes.scala Implemented boundingBox, might be some errors though Fixed the error in boundingBox, the values of width and height don't exist in the professors "Shape" import, so I altered the "Group" case to use the previous implementation of Location and Rectangle from shapes. Removed the comment brackets from all the tests to see if the program works now. Tests all passed for boundingBox, so now I created the 3 scala objects for size, height, and scale and initialized size. Initialized height. Initialized scale. Should mention; at this point AI has been used to ensure proper initialization of these 3 objects, and will be used some more now to test these objects and check the code Created all test suites for the 3 new objects and initialized TestSize. Initialized TestHeight Initialized TestScale Fixed an error Microsoft Copilot caught in the written tests for height where the tests expected "3" and "6" but I had written "2" and "4". Added logging to boundingBox.scala

--- src/main/scala/boundingBox.scala
+++ src/main/scala/boundingBox.scala
@@ -2,22 +2,28 @@ package edu.luc.cs.laufer.cs371.shapes
 
 // TODO: implement this behavior
 
+import java.util.logging.Logger
 import Shape.*
 
 object boundingBox:
+  private val log = Logger.getLogger("shapes.boundingBox")
   def apply(s: Shape): Location = s match
     //Rectangle
-    case Rectangle(w, h) => 
+    case Rectangle(w, h) =>
+      log.fine(s"boundingBox(Rectangle($w, $h))")
       Location(0, 0, Rectangle(w, h))
     //Ellipse
-    case Ellipse(rx, ry) => 
+    case Ellipse(rx, ry) =>
+      log.fine(s"boundingBox(Ellipse($rx, $ry))")
       Location(-rx, -ry, Rectangle(2 * rx, 2 * ry))
     //Location
-    case Location(x, y, inner) => 
+    case Location(x, y, inner) =>
+      log.fine(s"boundingBox(Location($x, $y, ...))")
       val Location(ix, iy, Rectangle(w, h)) = apply(inner)
       Location(ix + x, iy + y, Rectangle(w, h))
     //Group
     case Group(shapes*) =>
+      log.fine(s"boundingBox(Group(${shapes.size} shapes))")
       val boxes = shapes.map(apply)
       val minX = boxes.map(_.x).min
       val minY = boxes.map(_.y).min
