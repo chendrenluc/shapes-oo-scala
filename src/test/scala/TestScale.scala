ea48494146be04eaa73a6da00c5c6a26106f9676
chendren <connorhendren24@gmail.com>
2026-09-16 11:37:07 -0500

    Added missing cases to shapes.scala Implemented boundingBox, might be some errors though Fixed the error in boundingBox, the values of width and height don't exist in the professors "Shape" import, so I altered the "Group" case to use the previous implementation of Location and Rectangle from shapes. Removed the comment brackets from all the tests to see if the program works now. Tests all passed for boundingBox, so now I created the 3 scala objects for size, height, and scale and initialized size. Initialized height. Initialized scale. Should mention; at this point AI has been used to ensure proper initialization of these 3 objects, and will be used some more now to test these objects and check the code Created all test suites for the 3 new objects and initialized TestSize. Initialized TestHeight Initialized TestScale

--- src/test/scala/TestScale.scala
+++ src/test/scala/TestScale.scala
@@ -1,3 +1,37 @@
-class TestScale {
+package edu.luc.cs.laufer.cs371.shapes
 
+import org.scalatest.funsuite.AnyFunSuite
+import TestFixtures.*
+import scale.*
+import boundingBox.*
+import Shape.*
+
+class TestScale extends AnyFunSuite {
+  test("Scale Rectangle") {
+    val s = scale(simpleRectangle, 2)
+    val Location(x, y, Rectangle(w, h)) = boundingBox(s)
+    assert(w == 160)
+    assert(h == 240)
+  }
+  test("Scale Ellipse") {
+    val s = scale(simpleEllipse, 3)
+    val Location(x, y, Rectangle(w, h)) = boundingBox(s)
+    assert(w == 300)
+    assert(h == 180)
+  }
+  test("Scale Location") {
+    val s = scale(simpleLocation, 2)
+    val Location(x, y, Rectangle(w, h)) = boundingBox(s)
+    assert(x == 140)
+    assert(y == 60)
+    assert(w == 160)
+    assert(h == 240)
+  }
+  test("Scale Group") {
+    val s = scale(simpleGroup, 2)
+    val Location(x, y, Rectangle(w, h)) = boundingBox(s)
+    assert(w == 700)
+    assert(h == 560)
+  }
 }
+end TestScale
