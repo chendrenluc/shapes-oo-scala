bcd021a175f302b56d81da237981ce9b48a07418
chendren <connorhendren24@gmail.com>
2026-09-16 10:01:49 -0500

    Added missing cases to shapes.scala Implemented boundingBox, might be some errors though Fixed the error in boundingBox, the values of width and height don't exist in the professors "Shape" import, so I altered the "Group" case to use the previous implementation of Location and Rectangle from shapes. Removed the comment brackets from all the tests to see if the program works now. Tests all passed for boundingBox, so now I created the 3 scala objects for size, height, and scale and initialized size. Initialized height. Initialized scale. Should mention; at this point AI has been used to ensure proper initialization of these 3 objects, and will be used some more now to test these objects and check the code

--- src/main/scala/scale.scala
+++ src/main/scala/scale.scala
@@ -1,3 +1,16 @@
-object scale {
+package edu.luc.cs.laufer.cs371.shapes
 
+import Shape.*
+
+object scale {
+  def apply(s: Shape, factor: Int): Shape = s match {
+    case Rectangle(w, h) =>
+      Rectangle(w * factor, h * factor)
+    case Ellipse(rx, ry) =>
+      Ellipse(rx * factor, ry * factor)
+    case Location(x, y, inner) =>
+      Location(x * factor, y * factor, apply(inner, factor))
+    case Group(shapes*) =>
+      Group(shapes.map(apply(_, factor))*)
+  }
 }
